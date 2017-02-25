1f13c90a726a61c953114c3388483e5b72fcac25
Maor Stamati <maorstamati@gmail.com>
2017-02-25 23:49:12 +0200

    Add two numbers and print sum in otherLabel

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -19,8 +19,9 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        print(text1.text!)
-        print(text2.text!)
+       
+        otherLabel.text = "The sum is: \(Double(text1.text!)! + Double(text2.text!)!)"
+        
         
     }
     
