f14bdf7259cbbb078ba05674cba340715e75b9d4
Maor Stamati <maorstamati@gmail.com>
2017-02-25 23:27:34 +0200

    text fields

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -12,18 +12,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var otherLabel: UILabel!
 
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        otherLabel.text = "Hello there!"
-        tapCount = tapCount + 1
-        print(tapCount)
-        
-        if (tapCount == 10) {
-             otherLabel.text = "Tapped the button 10 times!"
-        }
-       
+        print(text1.text!)
+        print(text2.text!)
         
     }
     
